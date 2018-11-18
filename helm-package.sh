eab5ba33a73bc6bdbb1d20b57203e722fafad5b6
Jun Obama <george@yk.rim.or.jp>
2018-11-18 17:43:27 +0900

    helm package url is absolute

--- helm-package.sh
+++ helm-package.sh
@@ -8,7 +8,7 @@ set -ex
 mkdir -p ./helm-chart/charts
 pushd ./helm-chart/charts
 helm package ../kjwikigdocker
-helm repo index  .
+helm repo index  --url=https://raw.githubusercontent.com/george-pon/kjwikigdocker/master/helm-chart/charts/  .
 popd
 
 set +ex
