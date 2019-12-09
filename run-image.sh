007758215c5e7ec22a4144028273445999f1269e
Jun Obama <george@yk.rim.or.jp>
2019-12-09 14:34:56 +0900

    modify shell

--- run-image.sh
+++ run-image.sh
@@ -2,8 +2,8 @@
 #
 # test run image
 #
-docker pull georgesan/kjwikigdocker:latest
+docker pull kjwikigdocker:latest
 ${WINPTY_CMD} docker run --rm \
     -e http_proxy=${http_proxy} -e https_proxy=${https_proxy} -e no_proxy="${no_proxy}" \
     -p 8080:8080 \
-    georgesan/kjwikigdocker:latest
+    kjwikigdocker:latest
