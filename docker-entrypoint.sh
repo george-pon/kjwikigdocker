f16085860a657fb934e3ae2e75fe3c7a527db533
Jun Obama <george@yk.rim.or.jp>
2019-12-19 20:45:46 +0900

    fix entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,14 +3,15 @@
 # docker entrypoint
 #
 
-set -e
+set -ex
 
 if [ -n "$KJWikiG_WarFileName" ]; then
     if [ x"kjwikigdocker.war"x = x"$KJWikiG_WarFileName.war"x ]; then
         echo "KJWikiG_WarFileName value is default value.  skip rename."
     else
+        ls -laF $CATALINA_BASE/webapps/
         /bin/mv $CATALINA_BASE/webapps/kjwikigdocker.war  $CATALINA_BASE/webapps/$KJWikiG_WarFileName.war
-        /bin/sed -i -e '%response.sendRedirect("/kjwikigdocker/");%response.sendRedirect("/'$KJWikiG_WarFileName'/");%' $CATALINA_BASE/webapps/ROOT/index.jsp
+        /bin/sed -i -e 's%response.sendRedirect("/kjwikigdocker/");%response.sendRedirect("/'$KJWikiG_WarFileName'/");%g' $CATALINA_BASE/webapps/ROOT/index.jsp
     fi
 fi
 
